9f433e494f0d19f82e347d20fd506302abe5560f
Gustav Vinther-Jørgensen <gustavsvj@gmail.com>
2018-11-05 08:51:23 +0100

    Initial commit

---
+++ ObjectScanner/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include "cv.h" 
+#include "cxcore.h" 
+#include "highgui.h" 
+#include <conio.h> 
+
+int main(int argc, char* argv[]) {
+	printf("Hello World!\n");
+}
